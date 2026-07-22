5e77da5fd8fef72bf6ba3da9b9be8e56c98ae226
Gop-inath <gopinath07103@gmail.com>
2026-07-22 12:06:23 +0530

    Added side nav and backdrop blur for the UX when menu opened

--- web/src/components/MobileNav.tsx
+++ web/src/components/MobileNav.tsx
@@ -11,11 +11,34 @@ function MobileNav() {
             <h2 className="font-bebas text-primary text-2xl cursor-pointer">FRAMEBOX</h2>
 
             {/* MENU */}
-            {isOpen ? <Menu className="transition-colors duration-200 text-subtle hover:text-heading" onClick={() => setIsOpen(false)} />
+            {isOpen ?
+                <X className="transition-colors duration-200 text-subtle hover:text-heading z-40" onClick={() => setIsOpen(false)} />
                 :
-                <X className="transition-colors duration-200 text-subtle hover:text-heading" onClick={() => setIsOpen(true)} />
+                <Menu className="transition-colors duration-200 text-subtle hover:text-heading z-40" onClick={() => setIsOpen(true)} />
             }
+
+            {/* SIDE BAR AND THE NAV ITEMS */}
+            {isOpen && (
+                <div className={`fixed z-30 right-0 top-0 h-screen w-3/4 bg-navbar transform transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
+
+                    <ul className="transition-colors duration-300 text-md h-screen text-subtle font-sora flex flex-col justify-center items-center gap-12 xl:gap-8">
+                        <li className="transition-colors duration-300 hover:text-heading">Films</li>
+                        <li className="transition-colors duration-300 hover:text-heading">Create Account</li>
+                        <li className="transition-colors duration-300 hover:text-heading">Sign in</li>
+                    </ul>
+                </div>)}
+
+            {/* BLUR OVERLAY */}
+            {isOpen && (
+                <div
+                    className="fixed inset-0 bg-black/10 backdrop-blur-sm z-20"
+                    onClick={() => setIsOpen(false)}
+                />
+            )}
+
         </nav>
+
+
     );
 }
 
